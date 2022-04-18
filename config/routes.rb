525dcd723b880de2f59ab3373fd575f9eee83bf8
Shaun-Schwartz <33434280+Shaun-Schwartz@users.noreply.github.com>
2022-04-18 13:27:35 -0700

    Add activities controller with index and random actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'activities/index'
+  get 'activities/random'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'activities#random'
 end
