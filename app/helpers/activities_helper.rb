710545185027edc837beb7f9ddcf0872b6cc8b22
Shaun-Schwartz <33434280+Shaun-Schwartz@users.noreply.github.com>
2022-04-21 11:18:20 -0700

    Link to activity or google

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,10 @@
 module ActivitiesHelper
+  def link_to_activity(activity)
+    if activity.url
+      link_to activity.name, activity.url
+    else
+      google_base = 'https://www.google.com/search?q='
+      link_to activity.name, google_base + activity.name
+    end
+  end
 end
